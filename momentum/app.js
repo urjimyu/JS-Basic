49ffcd090e6187c90dc00fb9f3c3c82370657a8a
jimyu <aliyah_7@naver.com>
2023-03-10 01:15:03 +0900

    boolean, null, undefined

--- momentum/app.js
+++ momentum/app.js
@@ -1,13 +1,3 @@
-const a = 5;
-const b = "2";
-
-let myName = "jimyu";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello" + myName);
-
-myName = "jimeow";
-
-console.log("your new name is " + myName);
+const amIFat = null;
+let something;
+console.log(amIFat);
