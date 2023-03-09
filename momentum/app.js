4824906565ec285c249f90d6e9820de24eb8ce37
jimyu <aliyah_7@naver.com>
2023-03-10 02:05:08 +0900

    chores

--- momentum/app.js
+++ momentum/app.js
@@ -1,19 +1,3 @@
-function sayHi(nameOfPerson, age){
-    console.log("Hi my name is "+ nameOfPerson + "and I'm " + age);
-}
-
-sayHi("jimyu", 23);
-
-function plus(firstNum, secondNum) {
-    console.log(firstNum + secondNum);
-}
-
-function divide(firstNum, secondNum) {
-    console.log(firstNum / secondNum);
-}
-
-plus(3, 6);
-
 const player = {
     name: "jimyu",
     sayHi: function(otherPersonName){
