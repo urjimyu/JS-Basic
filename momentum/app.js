bd7037629b51e5f3e7817332096a67278d383291
jimyu <aliyah_7@naver.com>
2023-03-09 23:34:04 +0900

    const, let

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,13 @@
 const a = 5;
 const b = "2";
 
-const myName = "jimyu";
+let myName = "jimyu";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello" + myName);
+
+myName = "jimeow";
+
+console.log("your new name is " + myName);
