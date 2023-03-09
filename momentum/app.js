5e2775d823f0357be4fb2219d16aad6e8ebd4130
jimyu <aliyah_7@naver.com>
2023-03-10 01:38:22 +0900

    array

--- momentum/app.js
+++ momentum/app.js
@@ -1,3 +1,11 @@
-const amIFat = null;
-let something;
-console.log(amIFat);
+const daysOfWeek = ["mon", "tue", "wed", "thur", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek[5]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
