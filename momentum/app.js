3d93c19220b270a989c2d87252022c0423f89554
jimyu <aliyah_7@naver.com>
2023-03-15 22:48:22 +0900

    changing styles on JS

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,17 @@
-const title = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick(){
+    title.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    title.innerText("mouse is here!");
+}
+
+function handleMouseLeave(){
+    title.innerText="Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
