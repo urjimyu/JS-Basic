968f888b0478039d611f84d8b85bac40c85b4e09
jimyu <aliyah_7@naver.com>
2023-03-15 23:09:15 +0900

    event listening

--- momentum/app.js
+++ momentum/app.js
@@ -1,17 +1,38 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    title.style.color = "blue";
+    h1.style.color = "blue";
 }
 
 function handleMouseEnter(){
-    title.innerText("mouse is here!");
+    h1.innerText("mouse is here!");
 }
 
 function handleMouseLeave(){
-    title.innerText="Mouse is gone!";
+    h1.innerText="Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tamato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("It's all good.");
+}
+
+h1.onclick = handleTitleClick;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy ", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
