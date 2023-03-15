c1f5d174fb83e64e66db4e565ea1d9f6c05467ad
jimyu <aliyah_7@naver.com>
2023-03-15 23:51:32 +0900

    CSS in JS

--- momentum/app.js
+++ momentum/app.js
@@ -1,15 +1,11 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first=child h1");
 
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    } else{
-        newColor = "blue";
+    const clickedClass = "clicked";
+    if (h1.className === clickedClass){
+        h1.className = "";
+    } else {
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
 }
-
 h1.addEventListener("click", handleTitleClick);
