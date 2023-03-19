1094c61bbcaefc93bd831efe633f6f1b94be8831
jimyu <aliyah_7@naver.com>
2023-03-19 11:04:12 +0900

    events

--- momentum/app.js
+++ momentum/app.js
@@ -1,10 +1,17 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector(".login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
     console.log(username);
 }
 
+function handleLinkClick(event) {
+    alert("clicked!");
+}
+
 loginButton.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
