7236ad7c6f50ab62f78861ba7e847a0d6f53a0e4
jimyu <aliyah_7@naver.com>
2023-03-28 22:57:59 +0900

    created quotes arrays

---
+++ momentum/js/quotes.js
@@ -0,0 +1,47 @@
+const quotes = [
+{
+    quote: "The way to get started is to quit talking and begin doing."
+    author: "Walt Disney"
+},
+{
+    quote: "If you hit a wrong note, it's the next note that you play that determines if it's good or bad."
+    author: "Unknown"
+},
+{
+    quote: "누구나 재능은 있지만 드문 것은 그 재능이 이끄는 암흑 속으로 따라 들어갈 용기."
+    author: "Unknown"
+},
+{
+    quote: "To Travel is to Live",
+    author: "Hans Christian Andersen",
+},
+{
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
+},
+{
+    quote: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West",
+},
+{
+    quote: "Never go on trips with anyone you do not love.",
+    author: "Hemmingway",
+},
+{
+    quote: "We wander for distraction, but we travel for fulfilment.",
+    author: "Hilaire Belloc",
+},
+{
+    quote: "Travel expands the mind and fills the gap.",
+    author: "Sheda Savage",
+},
+{
+    quote: "Only a life lived for others is a life worthwhile.",
+    author: "Albert Einstein",
+}
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+console.log(quotes[0])
