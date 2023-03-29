275907847a0b41d29b3f0220cbda20a5981708e7
jimyu <aliyah_7@naver.com>
2023-03-29 23:43:01 +0900

    random quotes

--- momentum/js/quotes.js
+++ momentum/js/quotes.js
@@ -44,4 +44,7 @@ const quotes = [
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
-console.log(quotes[0])
+const saysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = saysQuote.quote;
+author.innerText= saysQuote.author;
