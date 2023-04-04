eabe5fa2f2b8b506cbff50e38ef098ab05e924fe
jimyu <aliyah_7@naver.com>
2023-04-04 23:07:58 +0900

    [Feat] deleteToDo

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -1,15 +1,20 @@
 const toDoForm =document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
 
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
+    span.innerText = newTodo;
     const button = document.createElement("button");
     button.innerText = "X";
     li.appendChild(span);
     li.appendChild(button);
-    span.innerText = newTodo;
     toDoList.appendChild(li);
 }
 
